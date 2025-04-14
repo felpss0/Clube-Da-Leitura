fc8a6f2a7ca3b58bc12bd0d85d4ab5e247554ef2
Filipe Straubel <fstraubel10@gmail.com>
2025-04-14 19:36:23 -0300

    Add project files.

---
+++ ClubeDaLeitura2.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClubeDaLeitura2.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
