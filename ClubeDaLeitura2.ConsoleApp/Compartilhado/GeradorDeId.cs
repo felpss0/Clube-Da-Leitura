df79a6c84c92850ea6f41049140d0e79c2c6b304
Filipe Straubel <fstraubel10@gmail.com>
2025-04-14 20:19:51 -0300

    Feito Classes gerais

---
+++ ClubeDaLeitura2.ConsoleApp/Compartilhado/GeradorDeId.cs
@@ -0,0 +1,39 @@
+﻿namespace ClubeDaLeitura2.ConsoleApp.Compartilhado
+{
+    class GeradorDeId
+    {
+        public static int IdAmigo = 0;
+        public static int IdCaixa = 0;
+        public static int IdRevista = 0;
+        public static int IdEmprestimo = 0;
+
+
+        public static int GerarIdAmigo()
+        {
+            IdAmigo++;
+
+            return IdAmigo;
+        }
+
+        public static int GerarIdCaixa()
+        {
+            IdCaixa++;
+
+            return IdCaixa;
+        }
+
+        public static int GerarIdRevista()
+        {
+            IdRevista++;
+
+            return IdRevista;
+        }
+
+        public static int GerarIdEmprestimo()
+        {
+            IdEmprestimo++;
+
+            return IdEmprestimo;
+        }
+    }
+}
